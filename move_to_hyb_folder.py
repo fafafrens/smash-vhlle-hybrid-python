858dff2ad6ec8b7d719771159755bfe6581da8be
AndrePalermo <andre.pappa95@gmail.com>
2023-05-03 22:31:39 +0200

    move with subprocess

--- move_to_hyb_folder.py
+++ move_to_hyb_folder.py
@@ -1,4 +1,4 @@
-import shutil
+import subprocess
 import sys
 
 #syntax to move the files from terminal $: python3 move_to_hyb_folder.py <path_to_hybrid_folder>
@@ -9,10 +9,10 @@ work_directory = '~'
 
 print("Friendly reminder: have you compiled the correct branches? If so, good for you :D")
 
-shutil.copy(f"{work_directory}/smash/build/smash",target_directory)
-shutil.copy(f"{work_directory}/super-vhlle/hlle_visc",target_directory)
-shutil.copy(f"{work_directory}/smash-hadron-sampler/build/sampler",target_directory)
-shutil.copy("./hybrid_lib.py",target_directory)
-shutil.copy("./Snakefile",target_directory)
-shutil.copy("./Snakefile_polarization",target_directory)
-shutil.copy("./plot_lib.py",target_directory)
+subprocess.run(["cp",f"{work_directory}/smash/build/smash",target_directory])
+subprocess.run(["cp",f"{work_directory}/super-vhlle/hlle_visc",target_directory])
+subprocess.run(["cp",f"{work_directory}/smash-hadron-sampler/build/sampler",target_directory])
+subprocess.run(["cp","./hybrid_lib.py",target_directory])
+subprocess.run(["cp","./Snakefile",target_directory])
+subprocess.run(["cp","./Snakefile_polarization",target_directory])
+subprocess.run(["cp","./plot_lib.py",target_directory])
